0503855f2fad6c13c0af54e90c852d2dce786113
Kat Marchán <kzm@zkat.tech>
2023-04-24 16:33:41 -0700

    feat(scripts): run lifecycle scripts in correct dependency order

--- crates/node-maintainer/src/linkers/mod.rs
+++ crates/node-maintainer/src/linkers/mod.rs
@@ -44,12 +44,12 @@ pub(crate) enum Linker {
 impl Linker {
     #[cfg(not(target_arch = "wasm32"))]
     pub fn isolated(opts: LinkerOptions) -> Self {
-        Self::Isolated(IsolatedLinker(opts))
+        Self::Isolated(IsolatedLinker::new(opts))
     }
 
     #[cfg(not(target_arch = "wasm32"))]
     pub fn hoisted(opts: LinkerOptions) -> Self {
-        Self::Hoisted(HoistedLinker(opts))
+        Self::Hoisted(HoistedLinker::new(opts))
     }
 
     #[allow(dead_code)]
@@ -88,17 +88,349 @@ impl Linker {
     #[cfg(not(target_arch = "wasm32"))]
     pub async fn rebuild(
         &self,
-        #[allow(dead_code)] graph: &Graph,
-        #[allow(dead_code)] ignore_scripts: bool,
+        graph: &Graph,
+        ignore_scripts: bool,
     ) -> Result<(), NodeMaintainerError> {
-        match self {
-            #[cfg(not(target_arch = "wasm32"))]
-            Self::Isolated(isolated) => isolated.rebuild(graph, ignore_scripts).await,
-            #[cfg(not(target_arch = "wasm32"))]
-            Self::Hoisted(hoisted) => hoisted.rebuild(graph, ignore_scripts).await,
-            Self::Null => Ok(()),
+        tracing::debug!("Running lifecycle scripts...");
+        let start = std::time::Instant::now();
+        if !ignore_scripts {
+            self.run_scripts(graph, "preinstall").await?;
         }
+        self.link_bins(graph).await?;
+        if !ignore_scripts {
+            self.run_scripts(graph, "install").await?;
+            self.run_scripts(graph, "postinstall").await?;
+        }
+        tracing::debug!(
+            "Ran lifecycle scripts in {}ms.",
+            start.elapsed().as_millis()
+        );
+        Ok(())
     }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    async fn link_bins(
+        &self,
+        #[allow(dead_code)] graph: &Graph,
+    ) -> Result<usize, NodeMaintainerError> {
+        tracing::debug!("Linking bins...");
+        let start = std::time::Instant::now();
+        let linked = match self {
+            #[cfg(not(target_arch = "wasm32"))]
+            Self::Isolated(isolated) => isolated.link_bins(graph).await,
+            #[cfg(not(target_arch = "wasm32"))]
+            Self::Hoisted(hoisted) => hoisted.link_bins(graph).await,
+            Self::Null => Ok(0),
+        }?;
+        tracing::debug!(
+            "Linked {linked} package bins in {}ms.",
+            start.elapsed().as_millis()
+        );
+        Ok(linked)
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    async fn run_scripts(
+        &self,
+        #[allow(dead_code)] graph: &Graph,
+        #[allow(dead_code)] event: &str,
+    ) -> Result<(), NodeMaintainerError> {
+        use std::io::{BufReader, BufRead};
+
+        use futures::{StreamExt, TryStreamExt};
+        use oro_common::BuildManifest;
+        use oro_script::OroScript;
+
+        let (pending_rebuild, opts) = match self {
+            #[cfg(not(target_arch = "wasm32"))]
+            Self::Isolated(isolated) => (&isolated.pending_rebuild, &isolated.opts),
+            #[cfg(not(target_arch = "wasm32"))]
+            Self::Hoisted(hoisted) => (&hoisted.pending_rebuild, &hoisted.opts),
+            Self::Null => return Ok(())
+        };
+        let start = std::time::Instant::now();
+        tracing::debug!("Running {event} lifecycle scripts");
+        let root = &opts.root;
+        futures::stream::iter(pending_rebuild.lock().await.iter().copied())
+            .map(Ok)
+            .try_for_each_concurrent(opts.script_concurrency, move |idx| async move {
+                let (pkg_dir, workspace_path) = if idx == graph.root {
+                    (root.clone(), root.clone())
+                } else {
+                    match self {
+                        #[cfg(not(target_arch = "wasm32"))]
+                        Self::Isolated(isolated) => isolated.package_dir(graph, idx),
+                        #[cfg(not(target_arch = "wasm32"))]
+                        Self::Hoisted(hoisted) => hoisted.package_dir(graph, idx),
+                        Self::Null => return Ok(())
+                    }
+                };
+
+                let is_optional = graph.is_optional(idx);
+
+                let build_mani =
+                    BuildManifest::from_path(pkg_dir.join("package.json")).map_err(|e| {
+                        NodeMaintainerError::BuildManifestReadError(pkg_dir.join("package.json"), e)
+                    })?;
+
+                let name = graph[idx].package.name().to_string();
+                if build_mani.scripts.contains_key(event) {
+                    let package_dir = pkg_dir.clone();
+                    let event = event.to_owned();
+                    let event_clone = event.clone();
+                    let span = tracing::info_span!("script");
+                    let _span_enter = span.enter();
+                    if let Some(on_script_start) = &opts.on_script_start {
+                        on_script_start(&graph[idx].package, &event);
+                    }
+                    std::mem::drop(_span_enter);
+                    let mut script = match async_std::task::spawn_blocking(move || {
+                        OroScript::new(package_dir, event_clone)?
+                            .workspace_path(workspace_path)
+                            .spawn()
+                    })
+                    .await
+                    {
+                        Ok(script) => script,
+                        Err(e) if is_optional => {
+                            let e: NodeMaintainerError = e.into();
+                            tracing::debug!("Error in optional dependency script: {}", e);
+                            return Ok(());
+                        }
+                        Err(e) => return Err(e.into()),
+                    };
+                    let stdout = script.stdout.take();
+                    let stderr = script.stderr.take();
+                    let stdout_name = name.clone();
+                    let stderr_name = name.clone();
+                    let stdout_on_line = opts.on_script_line.clone();
+                    let stderr_on_line = opts.on_script_line.clone();
+                    let stdout_span = span;
+                    let stderr_span = stdout_span.clone();
+                    let event_clone = event.clone();
+                    let join = futures::try_join!(
+                        async_std::task::spawn_blocking(move || {
+                            let _enter = stdout_span.enter();
+                            if let Some(stdout) = stdout {
+                                for line in BufReader::new(stdout).lines() {
+                                    let line = line?;
+                                    tracing::debug!("stdout::{stdout_name}::{event}: {}", line);
+                                    if let Some(on_script_line) = &stdout_on_line {
+                                        on_script_line(&line);
+                                    }
+                                }
+                            }
+                            Ok::<_, NodeMaintainerError>(())
+                        }),
+                        async_std::task::spawn_blocking(move || {
+                            let _enter = stderr_span.enter();
+                            if let Some(stderr) = stderr {
+                                for line in BufReader::new(stderr).lines() {
+                                    let line = line?;
+                                    tracing::debug!(
+                                        "stderr::{stderr_name}::{event_clone}: {}",
+                                        line
+                                    );
+                                    if let Some(on_script_line) = &stderr_on_line {
+                                        on_script_line(&line);
+                                    }
+                                }
+                            }
+                            Ok::<_, NodeMaintainerError>(())
+                        }),
+                        async_std::task::spawn_blocking(move || {
+                            script.wait()?;
+                            Ok::<_, NodeMaintainerError>(())
+                        }),
+                    );
+                    match join {
+                        Ok(_) => {}
+                        Err(e) if is_optional => {
+                            tracing::debug!("Error in optional dependency script: {}", e);
+                            return Ok(());
+                        }
+                        Err(e) => return Err(e),
+                    }
+                }
+
+                Ok::<_, NodeMaintainerError>(())
+            })
+            .await?;
+        tracing::debug!(
+            "Ran lifecycle scripts for {event} in {}ms.",
+            start.elapsed().as_millis()
+        );
+        Ok(())
+    }
+
+    // pub async fn run_scripts(&self, graph: &Graph, event: &str) -> Result<(), NodeMaintainerError> {
+    //     let root = &self.opts.root;
+    //     let pending = self
+    //         .pending_rebuild
+    //         .lock()
+    //         .await
+    //         .iter()
+    //         .copied()
+    //         .collect::<Vec<_>>();
+    //     let remaining_dependencies = Arc::new(Mutex::new(
+    //         pending
+    //             .iter()
+    //             .map(|idx| {
+    //                 let mut deps = HashSet::new();
+    //                 for dep in &pending {
+    //                     if petgraph::algo::has_path_connecting(&graph.inner, *idx, *dep, None) {
+    //                         deps.insert(*dep);
+    //                     }
+    //                 }
+    //                 (*idx, deps)
+    //             })
+    //             .collect::<HashMap<NodeIndex, HashSet<NodeIndex>>>(),
+    //     ));
+    //     let dependents = Arc::new(
+    //         pending
+    //             .iter()
+    //             .map(|idx| {
+    //                 let mut deps = HashSet::new();
+    //                 for dep in &pending {
+    //                     if petgraph::algo::has_path_connecting(&graph.inner, *dep, *idx, None) {
+    //                         deps.insert(*dep);
+    //                     }
+    //                 }
+    //                 (*idx, deps)
+    //             })
+    //             .collect::<HashMap<NodeIndex, HashSet<NodeIndex>>>(),
+    //     );
+    //     receiver
+    //         .map(Ok)
+    //         .try_for_each_concurrent(
+    //             self.opts.script_concurrency,
+    //             move |(idx, remaining_dependencies, dependents)| async move {
+    //                 let package_dir = if idx == graph.root {
+    //                     root.clone()
+    //                 } else {
+    //                     let subdir = graph
+    //                         .node_path(idx)
+    //                         .iter()
+    //                         .map(|x| x.to_string())
+    //                         .collect::<Vec<_>>()
+    //                         .join("/node_modules/");
+    //                     root.join("node_modules").join(subdir)
+    //                 };
+
+    //                 let is_optional = graph.is_optional(idx);
+
+    //                 let build_mani = BuildManifest::from_path(package_dir.join("package.json"))
+    //                     .map_err(|e| {
+    //                         NodeMaintainerError::BuildManifestReadError(
+    //                             package_dir.join("package.json"),
+    //                             e,
+    //                         )
+    //                     })?;
+
+    //                 let name = graph[idx].package.name().to_string();
+    //                 if build_mani.scripts.contains_key(event) {
+    //                     let package_dir = package_dir.clone();
+    //                     let root = root.clone();
+    //                     let event = event.to_owned();
+    //                     let event_clone = event.clone();
+    //                     let span = tracing::info_span!("script");
+    //                     let _span_enter = span.enter();
+    //                     if let Some(on_script_start) = &self.opts.on_script_start {
+    //                         on_script_start(&graph[idx].package, &event);
+    //                     }
+    //                     std::mem::drop(_span_enter);
+    //                     let mut script = match async_std::task::spawn_blocking(move || {
+    //                         OroScript::new(package_dir, event_clone)?
+    //                             .workspace_path(root)
+    //                             .spawn()
+    //                     })
+    //                     .await
+    //                     {
+    //                         Ok(script) => script,
+    //                         Err(e) if is_optional => {
+    //                             let e: NodeMaintainerError = e.into();
+    //                             tracing::debug!("Error in optional dependency script: {}", e);
+    //                             return Ok(());
+    //                         }
+    //                         Err(e) => return Err(e.into()),
+    //                     };
+    //                     let stdout = script.stdout.take();
+    //                     let stderr = script.stderr.take();
+    //                     let stdout_name = name.clone();
+    //                     let stderr_name = name.clone();
+    //                     let stdout_on_line = self.opts.on_script_line.clone();
+    //                     let stderr_on_line = self.opts.on_script_line.clone();
+    //                     let stdout_span = span;
+    //                     let stderr_span = stdout_span.clone();
+    //                     let event_clone = event.clone();
+    //                     let join = futures::try_join!(
+    //                         async_std::task::spawn_blocking(move || {
+    //                             let _enter = stdout_span.enter();
+    //                             if let Some(stdout) = stdout {
+    //                                 for line in BufReader::new(stdout).lines() {
+    //                                     let line = line?;
+    //                                     tracing::debug!("stdout::{stdout_name}::{event}: {}", line);
+    //                                     if let Some(on_script_line) = &stdout_on_line {
+    //                                         on_script_line(&line);
+    //                                     }
+    //                                 }
+    //                             }
+    //                             Ok::<_, NodeMaintainerError>(())
+    //                         }),
+    //                         async_std::task::spawn_blocking(move || {
+    //                             let _enter = stderr_span.enter();
+    //                             if let Some(stderr) = stderr {
+    //                                 for line in BufReader::new(stderr).lines() {
+    //                                     let line = line?;
+    //                                     tracing::debug!(
+    //                                         "stderr::{stderr_name}::{event_clone}: {}",
+    //                                         line
+    //                                     );
+    //                                     if let Some(on_script_line) = &stderr_on_line {
+    //                                         on_script_line(&line);
+    //                                     }
+    //                                 }
+    //                             }
+    //                             Ok::<_, NodeMaintainerError>(())
+    //                         }),
+    //                         async_std::task::spawn_blocking(move || {
+    //                             script.wait()?;
+    //                             Ok::<_, NodeMaintainerError>(())
+    //                         }),
+    //                     );
+    //                     match join {
+    //                         Ok(_) => {}
+    //                         Err(e) if is_optional => {
+    //                             tracing::debug!("Error in optional dependency script: {}", e);
+    //                             return Ok(());
+    //                         }
+    //                         Err(e) => return Err(e),
+    //                     }
+    //                 }
+
+    //                 if let Some(set) = dependents.get(&idx) {
+    //                     let mut remaining = remaining_dependencies.lock().await;
+    //                     for dep in set {
+    //                         if let Some(remaining) = remaining.get_mut(dep) {
+    //                             remaining.remove(&idx);
+    //                             if remaining.is_empty() {
+    //                                 sender_ref.unbounded_send((
+    //                                     *dep,
+    //                                     remaining_dependencies.clone(),
+    //                                     dependents.clone(),
+    //                                 ))?;
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+
+    //                 Ok::<_, NodeMaintainerError>(())
+    //             },
+    //         )
+    //         .await?;
+
+    //     Ok(())
+    // }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
